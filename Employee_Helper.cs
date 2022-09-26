7316dbf419acaf7c08528c46e33afcc0c2da631c
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-26 22:55:54 +0530

    Add files via upload

---
+++ Employee_Helper.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLogicLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_Library;
+
+namespace HelperLibrary
+{
+    
+    public class Employee_Helper
+    {
+        Employee_DAL dal = null;
+        public Employee_Helper()
+        {
+             dal = new Employee_DAL();
+        }
+        public bool AddEmployee(Employee_BAL employee)
+        {
+           return dal.InsertEmployee(employee);
+        }
+
+        public bool EditEmployee(Employee_BAL employee)
+        {
+            return dal.UpdateEmployee(employee);
+        }
+
+        public bool RemoveEmployee(Employee_BAL employee)
+        {
+            return dal.DeleteEmployee(employee_id);
+        }
+
+        public void SearchEmployee(Employee_BAL employee)
+        {
+            dal.FindEmployee(empid, out empdata);
+        }
+        public int countEmployees()
+        {
+            return dal.EmployeeCount();
+
+        }
+        public List<Employee_BAL> ShowEmployeeList()
+        {
+            return dal.EmployeeList();
+        }
+    }
+}
